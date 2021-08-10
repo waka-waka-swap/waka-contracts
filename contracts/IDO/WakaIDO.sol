511a9e8c3ee0617505f78422d8b6246022e15d24
Bespalov Vlad <bespalov-vlad99@mail.ru>
2021-08-10 14:08:19 +0300

    waka ido contracts

---
+++ contracts/IDO/WakaIDO.sol
@@ -0,0 +1,253 @@
+pragma solidity 0.6.12;
+
+import "./WakaTiers.sol";
+import "../utils/SafeERC20.sol";
+
+contract IFO is ReentrancyGuard {
+    uint constant MAX_NUM_TIERS = 10;
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    struct UserInfo {
+        uint256 firstRoundAmountToClaim;
+        uint256 secondRoundAmountToClaim;
+        uint256 thirdRoundAmountToClaim;
+        uint8 tier;
+        bool registered;
+        bool claimed;
+    }
+
+    address public adminAddress;
+    IERC20 public raisingToken;
+    IERC20 public offeringToken;
+    WakaTiers public tiersContract;
+
+    uint256 public registrationStartBlock;
+    uint256 public firstRoundStartBlock;
+    uint256 public firstRoundEndBlock;
+    uint256 public secondRoundEndBlock;
+    uint256 public holdTokensTillBlock;
+
+    // total amount of raising tokens need to be raised
+    uint256 public raisingAmount;
+    // total amount of offeringToken that will offer
+    uint256 public offeringAmount;
+    // address => amount
+    mapping (address => UserInfo) public userInfo;
+    // participators
+    address[] public addressList;
+
+    uint[MAX_NUM_TIERS] public numberOfUsersByTier;
+
+    uint256 public offeringAmountFistRoundTotal;
+    uint256 public offeringAmountSecondRoundTotal;
+    uint256 public offeringAmountThirdRoundTotal;
+
+    event Deposit(address indexed user, uint256 amount, uint8 round);
+    event Claim(address indexed user, uint256 offeringAmount);
+
+    constructor(
+        IERC20 _raisingToken,
+        IERC20 _offeringToken,
+        uint256 _registrationStartBlock,
+        uint256 _firstRoundStartBlock,
+        uint256 _firstRoundEndBlock,
+        uint256 _secondRoundEndBlock,
+        uint256 _holdTokensTillBlock,
+        uint256 _offeringAmount,
+        uint256 _raisingAmount,
+        address _adminAddress,
+        address _tiersContractAddress
+    ) public {
+        require(_registrationStartBlock < _firstRoundStartBlock, 'the first round block number is less than the registration start');
+        require(_firstRoundStartBlock < _firstRoundEndBlock, 'the first round end number is less than the start');
+        require(_firstRoundEndBlock < _secondRoundEndBlock, 'the first round end block is more than the second round start');
+        require(_secondRoundEndBlock < _holdTokensTillBlock, 'the second round end block is more than the hold till tokens block number');
+        raisingToken = _raisingToken;
+        offeringToken = _offeringToken;
+        registrationStartBlock = _registrationStartBlock;
+        firstRoundStartBlock = _firstRoundStartBlock;
+        firstRoundEndBlock = _firstRoundEndBlock;
+        secondRoundEndBlock = _secondRoundEndBlock;
+        holdTokensTillBlock = _holdTokensTillBlock;
+        offeringAmount = _offeringAmount;
+        raisingAmount = _raisingAmount;
+        adminAddress = _adminAddress;
+        tiersContract = WakaTiers(_tiersContractAddress);
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == adminAddress, "not an admin");
+        _;
+    }
+
+    modifier onlyRegistered() {
+        require (userInfo[msg.sender].registered, 'the user has not been registered');
+        _;
+    }
+
+    modifier onlyNotClaimed() {
+        require (userInfo[msg.sender].claimed, 'the user has already claimed');
+        _;
+    }
+
+    modifier notZeroAmount(uint256 _amount) {
+        require (_amount > 0, 'amount has to be greater than 0');
+        _;
+    }
+
+    function register() public {
+        require (block.number > registrationStartBlock, 'registration didn`t start');
+        require (block.number < firstRoundStartBlock, 'registration finished');
+        uint8 currentTier = tiersContract.getUserTier(msg.sender);
+        require (currentTier != 0, 'the user has no tier');
+        UserInfo registeredUser = userInfo[msg.sender];
+        if (registeredUser.registered) {
+            numberOfUsersByTier[registeredUser.tier]--;
+        }
+        userInfo[msg.sender].tier = currentTier;
+        userInfo[msg.sender].registered = true;
+    }
+
+    function getTierWeight(uint8 _tier) public view returns(uint256) {
+        return numberOfUsersByTier[_tier].mul(tiersContract.tierWeight[_tier]);
+    }
+
+    function getTierAllocation(uint8 _tier) public view returns(uint256) {
+        uint currentTierWeight = getTierWeight(_tier);
+        uint summaryWeight = currentTierWeight;
+        for (uint8 i = 1; i <= MAX_NUM_TIERS; i++) {
+            if (i == _tier) {
+                continue;
+            }
+            summaryWeight = summaryWeight.add(getTierWeight(i));
+        }
+        return currentTierWeight.mul(offeringAmount).div(summaryWeight);
+    }
+
+    function getTokensLeftInPool() public view returns(uint256) {
+        return offeringAmount
+        .sub(offeringAmountFistRoundTotal)
+        .sub(offeringAmountSecondRoundTotal)
+        .sub(offeringAmountThirdRoundTotal);
+    }
+
+    function getSecondRoundAllocation(uint256 _amount) public pure view returns(uint256) {
+        return _amount.div(2);
+    }
+
+    function getUserAllocation(address _user) public view onlyRegistered returns(uint256) {
+        return getTierAllocation(userInfo[_user].tier).div(numberOfUsersByTier[userInfo[_user].tier]);
+    }
+
+    function depositFirstRound(uint256 _amountToPay) public onlyRegistered notZeroAmount(_amountToPay) {
+        require (block.number > firstRoundStartBlock && block.number < firstRoundEndBlock, 'not the first round time');
+        uint256 userAllocation = getUserAllocation(msg.sender);
+        UserInfo user = userInfo[msg.sender];
+        uint256 offeringTokenLeftToBuy = userAllocation.sub(user.firstRoundAmountToClaim);
+        require (offeringTokenLeftToBuy > 0, 'no tokens left to buy');
+        uint256 amountToDeposit = _amountToPay;
+        uint256 offeringTokenReceived = _amountToPay.mul(offeringAmount).div(raisingAmount);
+        if (offeringTokenReceived < offeringTokenLeftToBuy) {
+            offeringTokenReceived = offeringTokenLeftToBuy;
+            amountToDeposit = offeringTokenLeftToBuy.mul(raisingAmount).div(offeringAmount);
+        }
+        raisingToken.safeTransferFrom(address(msg.sender), address(this), amountToDeposit);
+        if (user.firstRoundAmountToClaim == 0) {
+            addressList.push(address(msg.sender));
+        }
+        user.firstRoundAmountToClaim = user.firstRoundAmountToClaim.add(offeringTokenReceived);
+        offeringAmountFistRoundTotal = offeringAmountFistRoundTotal.add(offeringTokenReceived);
+        emit Deposit(msg.sender, amountToDeposit, 1);
+    }
+
+    function depositSecondRound(uint256 _amountToPay) public onlyRegistered notZeroAmount(_amountToPay) {
+        require (block.number > firstRoundEndBlock && block.number < secondRoundEndBlock, 'not the second round time');
+        uint256 userAllocation = getSecondRoundAllocation(getUserAllocation(msg.sender));
+        UserInfo user = userInfo[msg.sender]; // TODO: fix storage
+        uint256 offeringTokenLeftToBuy = userAllocation.sub(user.secondRoundAmountToClaim);
+        uint256 tokensLeftInPool = getTokensLeftInPool();
+        require (offeringTokenLeftToBuy > 0 && tokensLeftInPool > 0, 'no tokens left to buy');
+        uint256 amountToDeposit = _amountToPay;
+        uint256 offeringTokenReceived = _amountToPay.mul(offeringAmount).div(raisingAmount);
+
+        if (offeringTokenReceived < offeringTokenLeftToBuy) {
+            offeringTokenReceived = offeringTokenLeftToBuy;
+        }
+
+        if (offeringTokenReceived < tokensLeftInPool) {
+            offeringTokenLeftToBuy = tokensLeftInPool;
+        }
+        amountToDeposit = offeringTokenReceived.mul(raisingAmount).div(offeringAmount);
+
+        raisingToken.safeTransferFrom(address(msg.sender), address(this), amountToDeposit);
+        bool userDidntDeposit = user.firstRoundAmountToClaim == 0 && user.secondRoundAmountToClaim == 0;
+        if (userDidntDeposit) {
+            addressList.push(address(msg.sender));
+        }
+        user.secondRoundAmountToClaim = user.secondRoundAmountToClaim.add(offeringTokenReceived);
+        offeringAmountSecondRoundTotal = offeringAmountSecondRoundTotal.add(offeringTokenReceived);
+        emit Deposit(msg.sender, amountToDeposit, 2);
+    }
+
+    function depositThirdRound(uint256 _amountToPay) public onlyRegistered notZeroAmount(_amountToPay) onlyNotClaimed {
+        require (block.number > secondRoundEndBlock, 'not the second round time');
+        uint256 tokensLeftInPool = getTokensLeftInPool();
+        require (tokensLeftInPool > 0, 'no tokens left in the pool');
+        uint256 amountToDeposit = _amountToPay;
+        uint256 offeringTokenReceived = _amountToPay.mul(offeringAmount).div(raisingAmount);
+
+        if (offeringTokenReceived < tokensLeftInPool) {
+            offeringTokenReceived = tokensLeftInPool;
+            amountToDeposit = offeringTokenReceived.mul(raisingAmount).div(offeringAmount);
+        }
+        raisingToken.safeTransferFrom(address(msg.sender), address(this), amountToDeposit);
+        bool userDidntDeposit = user.firstRoundAmountToClaim == 0
+        && user.secondRoundAmountToClaim == 0
+        && user.thirdRoundAmountToClaim == 0;
+        if (userDidntDeposit) {
+            addressList.push(address(msg.sender));
+        }
+        user.thirdRoundAmountToClaim = user.thirdRoundAmountToClaim.add(offeringTokenReceived);
+        offeringAmountThirdRoundTotal = offeringAmountThirdRoundTotal.add(offeringTokenReceived);
+        emit Deposit(msg.sender, amountToDeposit, 3);
+    }
+
+    function setOfferingAmount(uint256 _offerAmount) public onlyAdmin {
+        require (block.number < firstRoundStartBlock, 'first round has been already started');
+        offeringAmount = _offerAmount;
+    }
+
+    function setRaisingAmount(uint256 _raisingAmount) public onlyAdmin {
+        require (block.number < firstRoundStartBlock, 'first round has been already started');
+        raisingAmount= _raisingAmount;
+    }
+
+    function claim() public nonReentrant onlyRegistered onlyNotClaimed {
+        require (block.number > holdTokensTillBlock, 'claim has not been started');
+        UserInfo user = userInfo[msg.sender];
+        uint256 offeringTokenAmount = user.firstRoundAmountToClaim
+        .add(user.secondRoundAmountToClaim)
+        .add(user.thirdRoundAmountToClaim);
+        require (offeringTokenAmount > 0, 'you didn`t participate');
+
+        offeringToken.safeTransfer(address(msg.sender), offeringTokenAmount);
+        user.claimed = true;
+        emit Claim(msg.sender, offeringTokenAmount);
+    }
+
+    function isUserClaimed(address _user) external view returns(bool) {
+        return userInfo[_user].claimed;
+    }
+
+    function getAddressListLength() external view returns(uint256) {
+        return addressList.length;
+    }
+
+    function finalWithdraw(uint256 _lpAmount, uint256 _offerAmount) public onlyAdmin {
+        require (_lpAmount < raisingToken.balanceOf(address(this)), 'not enough token 0');
+        require (_offerAmount < offeringToken.balanceOf(address(this)), 'not enough token 1');
+        raisingToken.safeTransfer(address(msg.sender), _lpAmount);
+        offeringToken.safeTransfer(address(msg.sender), _offerAmount);
+    }
+}
---
+++ contracts/IDO/WakaTiers.sol
@@ -0,0 +1,164 @@
+pragma solidity 0.6.12;
+
+import "../utils/ReentrancyGuard.sol";
+import "../utils/Ownable.sol";
+import "../utils/SafeMath.sol";
+import "../utils/ERC20.sol";
+
+
+contract WakaTiers is Ownable, ReentrancyGuard {
+
+    using SafeMath for uint;
+
+    struct UserInfo {
+        uint staked;
+        uint stakedTime;
+    }
+
+    uint constant MAX_NUM_TIERS = 10;
+    uint8 currentMaxTier = 4;
+
+    mapping(address => UserInfo) public userInfo;
+    uint[MAX_NUM_TIERS] public tierPrice;
+    uint[MAX_NUM_TIERS] public tierWeight;
+
+    uint[] public withdrawFeePercent;
+    ERC20 public DUCK;
+
+    bool public canEmergencyWithdraw;
+
+    event Staked(address indexed user, uint amount);
+    event Withdrawn(address indexed user, uint indexed amount, uint fee);
+    event EmergencyWithdrawn(address indexed user, uint amount);
+
+    constructor(address _duckTokenAddress) public {
+        DUCK = ERC20Burnable(_duckTokenAddress);
+
+        tierPrice[1] = 2000e18;
+        tierPrice[2] = 5000e18;
+        tierPrice[3] = 10000e18;
+        tierPrice[4] = 20000e18;
+
+        tierWeight[1] = 1;
+        tierWeight[2] = 2;
+        tierWeight[3] = 5;
+        tierWeight[4] = 10;
+
+        withdrawFeePercent.push(30);
+        withdrawFeePercent.push(25);
+        withdrawFeePercent.push(20);
+        withdrawFeePercent.push(10);
+        withdrawFeePercent.push(5);
+        withdrawFeePercent.push(0);
+    }
+
+    function deposit(uint _amount) external nonReentrant() {
+        DUCK.transferFrom(msg.sender, address(this), _amount);
+
+        userInfo[msg.sender].staked = userInfo[msg.sender].staked.add(_amount);
+        userInfo[msg.sender].stakedTime = block.timestamp;
+
+        emit Staked(msg.sender, _amount);
+    }
+
+    function withdraw(uint _amount) external nonReentrant() {
+        UserInfo storage user = userInfo[msg.sender];
+        require(user.staked >= _amount, "not enough amount to withdraw");
+
+        uint toBurn = calculateWithdrawFee(msg.sender, _amount);
+        user.staked = user.staked.sub(_amount);
+
+        if(toBurn > 0) {
+            DUCK.burn(toBurn);
+        }
+
+        DUCK.transfer(msg.sender, _amount.sub(toBurn));
+        emit Withdrawn(msg.sender, _amount, toBurn);
+    }
+
+    function updateEmergencyWithdrawStatus(bool _status) external onlyOwner {
+        canEmergencyWithdraw = _status;
+    }
+
+    function emergencyWithdraw() external {
+        require(canEmergencyWithdraw, "function disabled");
+        UserInfo storage user = userInfo[msg.sender];
+        require(user.staked > 0, "nothing to withdraw");
+
+        uint _amount = user.staked;
+        user.staked = 0;
+
+        DUCK.transfer(msg.sender, _amount);
+        emit EmergencyWithdrawn(msg.sender, _amount);
+    }
+
+    function updateTierPrice(uint8 _tierId, uint _amount) external onlyOwner {
+        require(_tierId > 0 && _tierId <= MAX_NUM_TIERS, "invalid _tierId");
+        tierPrice[_tierId] = _amount;
+        if (_tierId > currentMaxTier) {
+            currentMaxTier = _tierId;
+        }
+    }
+
+    function updateTierWeight(uint8 _tierId, uint _amount) external onlyOwner {
+        require(_tierId > 0 && _tierId <= MAX_NUM_TIERS, "invalid _tierId");
+        tierWeight[_tierId] = _amount;
+        if (_tierId > currentMaxTier) {
+            currentMaxTier = _tierId;
+        }
+    }
+
+    function updateWithdrawFee(uint _key, uint _percent) external onlyOwner {
+        require(_percent < 100, "too high percent");
+        withdrawFeePercent[_key] = _percent;
+    }
+
+    function getUserTier(address _userAddress) external view returns(uint8 res) {
+        for (uint8 i = 1; i <= MAX_NUM_TIERS; i++) {
+            if(tierPrice[i] == 0 || userInfo[_userAddress].staked < tierPrice[i]) {
+                return res;
+            }
+
+            res = i;
+        }
+    }
+
+    function calculateWithdrawFee(address _userAddress, uint _amount) public view returns(uint) {
+        UserInfo storage user = userInfo[_userAddress];
+        require(user.staked >= _amount, "not enough amount to withdraw");
+
+        if(block.timestamp < user.stakedTime.add(10 days)) {
+            return _amount.mul(withdrawFeePercent[0]).div(100); //30%
+        }
+
+        if(block.timestamp < user.stakedTime.add(20 days)) {
+            return _amount.mul(withdrawFeePercent[1]).div(100); //25%
+        }
+
+        if(block.timestamp < user.stakedTime.add(30 days)) {
+            return _amount.mul(withdrawFeePercent[2]).div(100); //20%
+        }
+
+        if(block.timestamp < user.stakedTime.add(60 days)) {
+            return _amount.mul(withdrawFeePercent[3]).div(100); //10%
+        }
+
+        if(block.timestamp < user.stakedTime.add(90 days)) {
+            return _amount.mul(withdrawFeePercent[4]).div(100); //5%
+        }
+
+        return _amount.mul(withdrawFeePercent[5]).div(100);
+    }
+
+    //frontend func
+    function getTiers() external view returns(uint[MAX_NUM_TIERS] memory buf) {
+        for(uint8 i = 1; i < MAX_NUM_TIERS; i++) {
+            if(tierPrice[i] == 0) {
+                return buf;
+            }
+            buf[i-1] = tierPrice[i];
+        }
+
+        return buf;
+    }
+}
